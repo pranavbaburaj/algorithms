2999693c698e9195d24761db39010347cec72333
Pranav Baburaj <70764593+pranavbaburaj@users.noreply.github.com>
2020-12-19 15:30:26 +0530

    Create main.ts

---
+++ Fibonacci_Pattern/main.ts
@@ -0,0 +1,18 @@
+interface Question {
+	value : number
+}
+
+function fibonacci(data:Question):number {
+	var pattern:Array<number> = new Array()
+	var x:number = 0;
+
+	while (x < data.value + 1) {
+		if (x == 0 || x == 1) {
+			pattern.push(x)
+		} else {
+			pattern.push(pattern[x - 1] + pattern[x - 2])
+		}
+		x++;
+	}
+	return pattern
+}
