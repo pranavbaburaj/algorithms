f2859cb55cfb8698a322bbaf9063f9ab1a7acd2e
Pranav Baburaj <70764593+pranavbaburaj@users.noreply.github.com>
2021-01-23 20:11:28 +0530

    Create fib.dart

---
+++ Fibonacci_Pattern/fib.dart
@@ -0,0 +1,25 @@
+int fib(int n) {
+  var arr = [];
+  int i = 0;
+  
+  while (i < n + 1){
+    if (i == 0 || i == 1){ arr.add(i);}
+    else {
+      arr.add(arr[i - 1] + arr[i - 2]);
+    }
+    i += 1;
+  }
+  return arr[arr.length - 1];
+}
+
+int fibo(int n) {
+  if (n == 0 || n == 1) {return n;}
+  else {
+    return fibo(n - 1) + fibo(n - 2);
+  }
+}
+
+void main() {
+  print(fib(6));
+  print(fibo(6));
+}
